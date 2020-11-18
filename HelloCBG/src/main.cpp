16d9fd805b834d3cb697613400e6df3d984d8d4e
y-okumura <y-okumura@isp.co.jp>
2020-11-18 16:05:51 +0900

    Use the latest API(exec->add_callback_group)

--- HelloCBG/src/main.cpp
+++ HelloCBG/src/main.cpp
@@ -13,11 +13,11 @@ public:
   MinimalPubSub()
   : Node("minimal_pubsub"), count_(0)
   {
-    auto cb = create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+    cb_ = create_callback_group(rclcpp::CallbackGroupType::Reentrant);
 
     // publisher & its timer
     rclcpp::PublisherOptionsWithAllocator<AllocatorT> pub_options;
-    pub_options.callback_group = cb;
+    pub_options.callback_group = cb_;
 
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10, pub_options);
     auto timer_callback =
@@ -27,11 +27,11 @@ public:
         RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
         this->publisher_->publish(message);
       };
-    timer_ = this->create_wall_timer(500ms, timer_callback, cb);
+    timer_ = this->create_wall_timer(500ms, timer_callback, cb_);
 
     // subscriber
     rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> sub_options;
-    sub_options.callback_group = cb;
+    sub_options.callback_group = cb_;
     subscription_ = this->create_subscription<std_msgs::msg::String>(
       "topic",
       10,
@@ -41,10 +41,15 @@ public:
       sub_options);
   }
 
+  rclcpp::CallbackGroup::SharedPtr get_callback_group() {
+    return cb_;
+  }
+
 private:
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::CallbackGroup::SharedPtr cb_;
   size_t count_;
 };
 
@@ -53,9 +58,10 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<MinimalPubSub<>>();
+  auto cb = node->get_callback_group();
 
   auto exec = std::make_shared<rclcpp::executors::StaticSingleThreadedExecutor>();
-  exec->add_node(node);
+  exec->add_callback_group(cb, node->get_node_base_interface());
 
   exec->spin();
   rclcpp::shutdown();
